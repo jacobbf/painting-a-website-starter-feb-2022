439c94763704fc3c2de2be0ed7c611c9e56c78ca
Jake Brussel Faria <jake@jakebf.com>
2022-02-24 23:04:40 -0500

    Removed esbuild, bindery, content

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,9 @@
-const EsBuild = require('esbuild');
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./src");
   
   eleventyConfig.addPassthroughCopy({
     "src/public": "/"
   });
-
-  eleventyConfig.on('afterBuild', () => {
-    EsBuild.buildSync({
-      entryPoints: ["./src/scripts/index.js"],
-      bundle: true,
-      outfile: "_site/index.js",
-    });
-  })
 
   return {
     dir: {
